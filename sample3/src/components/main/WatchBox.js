0ff83b022ee6e7f7757ec7ef65999e9fea8a0454
pelshad <akuaru@gmail.com>
2024-05-14 16:59:27 +0900

    끄아아

--- sample3/src/components/main/WatchBox.js
+++ sample3/src/components/main/WatchBox.js
@@ -14,6 +14,7 @@ export default function WatchBox({ select }) {
                     <li>
                         <div className="img">
                             <img src={item.src} alt="" />
+                            {/* <div className="detail">설명</div> */}
                         </div>
                         <h4 className="name">{item.name}</h4>
                         <p className="price">$ {item.price}</p>
@@ -29,14 +30,27 @@ const CateBox = styled.ul`
     display: flex;
     gap: 30px;
     padding: 0px 20px;
+    width: 90%;
+    margin: 0 auto;
+    cursor: default;
     li {
         text-align: center;
         overflow: hidden;
         .img {
             width: 400px;
-            height: 300px;
-            border-radius: 15px;
+            height: 250px;
+            overflow: hidden;
         }
+        /* .img .detail {
+            position: relative;
+            transform: translateY(0%);
+            background: rgba(255, 255, 255, 0.5);
+
+            height: 300px;
+            z-index: 5;
+            transition: 0.5s all;
+        } */
+
         .img img {
             width: 100%;
             height: 100%;
@@ -44,9 +58,14 @@ const CateBox = styled.ul`
         h4 {
             font-size: 24px;
             margin: 10px 0;
+            color: #ddd;
         }
         .price {
             font-size: 18px;
+            color: #ddd;
         }
     }
+    /* li:hover .img .detail {
+        transform: translateY(-20%);
+    } */
 `;
