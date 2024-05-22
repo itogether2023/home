b95e15c7ea5fe7a9a6a277e0e19a4b740d528d96
pelshad <akuaru@gmail.com>
2024-05-22 14:11:41 +0900

    머지

--- sample4/src/components/Header.js
+++ sample4/src/components/Header.js
@@ -1,42 +1,63 @@
 import React, { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
 import styled from "styled-components";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import logo from "../asset/img/header/logo.png";
 
 export default function Header(props) {
+    const location = useLocation();
+    const [page, setPage] = useState("");
+
+    useEffect(() => {
+        setPage(location.pathname.substring(1).toUpperCase());
+    }, []);
+
     const xyHandler = (e, linkTarget) => {
         props.xyHandler(e, linkTarget);
     };
+
     return (
         <Nav>
             <div className="menuIcon">
-                <i class="fas fa-ellipsis-v"></i>
+                <span>
+                    {page === "" ? <i class="fas fa-ellipsis-v"></i> : page}
+                </span>
             </div>
             <ul>
                 <li
                     onClick={(e) => {
-                        xyHandler(e, "About");
+                        xyHandler(e, "/");
+                    }}
+                >
+                    main
+                </li>
+                <li
+                    onClick={(e) => {
+                        xyHandler(e, "about");
                     }}
                 >
                     About
                 </li>
                 <li
                     onClick={(e) => {
-                        xyHandler(e, "Portfolio");
+                        xyHandler(e, "portfolio");
                     }}
                 >
                     Portfolio
                 </li>
                 <li
                     onClick={(e) => {
-                        xyHandler(e, "About");
+                        xyHandler(e, "about");
                     }}
                 >
                     menu
                 </li>
                 <li
                     onClick={(e) => {
-                        xyHandler(e, "Contact");
+                        xyHandler(e, "contact");
                     }}
                 >
                     Contact
@@ -47,51 +68,75 @@ export default function Header(props) {
 }
 
 const Nav = styled.nav`
+    border: 2px dashed white;
     position: fixed;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    width: 5%;
-    height: 10%;
-    right: 0%;
+    width: 10%;
+    height: 5%;
+    transform: translateX(-50%);
+    left: 50%;
     z-index: 2;
-    top: 10%;
-    transform: translate(-50%, -50%);
-    border-radius: 25%; /* background: #101010; */
-    background: #262626;
+    border-radius: 20px;
+    background: rgba(0, 0, 0, 0.8);
+    overflow: hidden;
     transition: 1s all;
-
+    animation: FDown 1s forwards;
+    top: 3%;
+    opacity: 0;
+    @keyframes FDown {
+        from {
+            top: -10%;
+        }
+        to {
+            opacity: 1;
+            top: 3%;
+        }
+    }
     .menuIcon {
         text-align: center;
-        margin-top: 30%;
-        i {
+        span {
             color: white;
-            font-size: 36px;
+            font-size: 24px;
             transition: 1s all;
+            line-height: 1.45;
+            cursor: pointer;
+        }
+        span i {
             transform: rotate(90deg);
         }
     }
     ul {
         margin-bottom: 20%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        margin-top: 10px;
         li {
             cursor: default;
-            display: block;
             font-size: 28px;
             padding: 0 20px;
             color: #999999;
             /* transform: translateX(100%); */
             transition: 1s all;
-            margin-bottom: 50px;
             opacity: 0;
         }
     }
 
     &:hover {
-        width: 15%;
+        height: 40%;
+        width: 25%;
+        background: rgba(0, 0, 0, 1);
         .menuIcon {
+            span {
+                font-size: 32px;
+            }
             i {
-                transform: rotate(90deg);
                 opacity: 0;
+                transform: rotate(90deg);
             }
         }
         ul {
